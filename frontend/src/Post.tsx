d76e80b3f786d74a53fd453951a9c402b17b8e59
Shashwat Saxena <shashwat.saxena612@gmail.com>
2024-03-15 19:04:23 +0530

    some further improvments done

--- frontend/src/Post.tsx
+++ frontend/src/Post.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { useParams } from "react-router-dom";
 
